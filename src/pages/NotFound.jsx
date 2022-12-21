4340a73677885ca509206477f98d2cfdacffeae8
jrparente <jrparente@gmail.com>
2022-12-21 15:48:10 +0000

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,23 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Paragraph, Section, Title } from "../components/Theme/Utils";
 
 export default function NotFound() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 3000);
+  }, []);
+
   return (
     <>
       <Section>
         <Title>404 - Not Found</Title>
         <Paragraph>
           Looks like the page you were looking for decided to take a break! 🦥
-          Try navigating to another page 👆
+          You will be redirected to the homepage soon!
         </Paragraph>
       </Section>
     </>
