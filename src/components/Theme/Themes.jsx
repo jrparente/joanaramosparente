ed74596d73cd64dbbf1236560df5a22f8f3ddc39
jrparente <jrparente@gmail.com>
2022-12-21 14:55:24 +0000

    Update Themes.jsx

--- src/components/Theme/Themes.jsx
+++ src/components/Theme/Themes.jsx
@@ -16,7 +16,7 @@ export const darkTheme = {
   body: "#111111",
   text: "#ffffff",
   background: "#555555",
-  accent: "#FDBFBF",
+  accent: "#A30049",
   highlight: `linear-gradient(
     to right,
     rgba(255, 255, 0, 0.3) 0%,
