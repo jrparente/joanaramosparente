3ce6d55a6a9be58df81b65b1ed0e09110e651822
jrparente <jrparente@gmail.com>
2022-12-23 09:38:32 +0000

    Update CertificationData.js

--- src/pages/Certifications/CertificationData.js
+++ src/pages/Certifications/CertificationData.js
@@ -3,7 +3,7 @@ export default [
     id: 1,
     name: "SheCodes React",
     link: "https://www.shecodes.io/certificates/a0b7743cb1db817f49bab21263c8da22",
-    imgURL: "./public/SheCodes-React.png",
+    imgURL: "./SheCodes-React.png",
     finalProject: {
       gitHub: "",
       liveDemo: "https://shecodes-react-weather-app-jp.netlify.app",
@@ -13,7 +13,7 @@ export default [
     id: 2,
     name: "SheCodes Responsive",
     link: "https://www.shecodes.io/certificates/853f2f3267658d32f729ff47686db890",
-    imgURL: "./public/SheCodes-Responsive.png",
+    imgURL: "./SheCodes-Responsive.png",
     finalProject: {
       gitHub: "",
       liveDemo: "",
@@ -23,7 +23,7 @@ export default [
     id: 3,
     name: "SheCodes Plus",
     link: "https://www.shecodes.io/certificates/f0b05f93503de0ee1f89615a20e22233",
-    imgURL: "./public/SheCodes-Plus.png",
+    imgURL: "./SheCodes-Plus.png",
     finalProject: {
       gitHub: "",
       liveDemo: "https://weather.joanaramosparente.pt/",
@@ -33,7 +33,7 @@ export default [
     id: 4,
     name: "SheCodes Basics",
     link: "https://www.shecodes.io/certificates/28381515327395c3c315930213104caf",
-    imgURL: "./public/SheCodes-Basics.png",
+    imgURL: "./SheCodes-Basics.png",
     finalProject: {
       gitHub: "",
       liveDemo: "",
@@ -43,7 +43,7 @@ export default [
     id: 5,
     name: "freeCodeCamp Responsive Web Design",
     link: "https://www.freecodecamp.org/certification/jrparente/responsive-web-design",
-    imgURL: "./public/freeCodeCampResponsiveWD.png",
+    imgURL: "./freeCodeCampResponsiveWD.png",
     finalProject: {
       gitHub: "",
       liveDemo: "",
